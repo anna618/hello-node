4fdca907acbf93b5ac42f9076db019054c85e289
Anna Liu <a86061827@gmail.com>
2021-10-18 01:32:19 +0800

    2021/10/18 更新所有檔案

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,9 +1,19 @@
 const axios = require("axios");
 
+let format = "json";
+let today = "20211017";
+let stockCode = "0056";
+
+// https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=${format}&date=${today}&stockNo=${stockCode}
+
 axios
-  .get(
-    "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20211017&stockNo=0050&_=1634438682049"
-  )
+  .get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`, {
+    params: {
+      response: format,
+      date: today,
+      stockNo: stockCode,
+    },
+  })
   .then((response) => {
     console.log(response.data);
   })
